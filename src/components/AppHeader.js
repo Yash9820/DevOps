af748b2920fd14c0b6f1ec21e025bf607b2fe413
Yash Patel <yash.cba1732@ict.gnu.ac.in>
2020-01-20 03:17:34 +0000

    commit3

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 function AppHeader() {
 
     return <div>
-        <p className="p-3 text-light h3 bg-primary">Ganpat Chganges start yash</p>
+        <p className="p-3 text-light h3 bg-primary">Ganpat Chganges  yash patel</p>
     </div>
 }
   
